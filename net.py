e882a06a463866fdcee83aaca3dc871a0d7e41cd
nicolacucina <nicola.cucina@studenti.unipg.it>
2024-05-31 12:02:51 +0200

    changed gitignore

--- net.py
+++ net.py
@@ -19,16 +19,16 @@ class Net(nn.Module):
                 nn.BatchNorm2d(64),
                 nn.ReLU()
             )
-            # self.conv2 = nn.Sequential(
-            #     nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=(1, 1), stride=(2, 2)),
-            #     nn.BatchNorm2d(128),
-            #     nn.ReLU()
-            # )
-            # self.conv3 = nn.Sequential(
-            #     nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=(1, 1), stride=(1, 1)),
-            #     nn.BatchNorm2d(128),
-            #     nn.ReLU()
-            # )
+            self.conv2 = nn.Sequential(
+                nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=(1, 1), stride=(2, 2)),
+                nn.BatchNorm2d(128),
+                nn.ReLU()
+            )
+            self.conv3 = nn.Sequential(
+                nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=(1, 1), stride=(1, 1)),
+                nn.BatchNorm2d(128),
+                nn.ReLU()
+            )
             # self.conv4 = nn.Sequential(
             #     nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), padding=(1, 1), stride=(2, 2)),
             #     nn.BatchNorm2d(256),
@@ -39,7 +39,7 @@ class Net(nn.Module):
             #     nn.BatchNorm2d(256),
             #     nn.ReLU()
             # )
-            self.fc = nn.Linear(20 * 20 * 64, 2)
+            self.fc = nn.Linear(20 * 20 * 32, 2)
         if self.type == 'fully_connected':
             #TODO
             return
@@ -56,8 +56,8 @@ class Net(nn.Module):
         if self.type == 'convolutional':
             out = self.conv1(x)
             out = self.conv1_1(out)
-            # out = self.conv2(out)
-            # out = self.conv3(out)
+            out = self.conv2(out)
+            out = self.conv3(out)
             # out = self.conv4(out)
             # out = self.conv5(out)
             out = out.view(out.size(0), -1)
